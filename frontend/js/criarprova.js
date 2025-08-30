307faf88bf74dc50747d4f8d35c4946c9b14de44
Samuel <reis68535@gmail.com>
2025-08-30 01:05:14 -0300

    so uns errinho de logout ja resolvido

--- frontend/js/criarprova.js
+++ frontend/js/criarprova.js
@@ -284,36 +284,10 @@ function saveExam() {
     sendExamToServer();
 }
 
-// Função para enviar a prova ao servidor
+
 function sendExamToServer() {
-    // Simulação de envio para o servidor
+
     console.log('Dados da prova a serem enviados:', examData);
-    
-    // Aqui você faria a requisição AJAX para o servidor
-    // Exemplo com fetch:
-    /*
-    fetch('/api/provas', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(examData)
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            alert('Prova salva com sucesso!');
-            // Limpar formulário ou redirecionar
-        } else {
-            alert('Erro ao salvar prova: ' + data.message);
-        }
-    })
-    .catch(error => {
-        console.error('Erro:', error);
-        alert('Erro ao conectar com o servidor.');
-    });
-    */
-    
-    // Simulando sucesso para demonstração
+
     alert('Prova salva com sucesso! Em uma implementação real, os dados seriam enviados ao servidor.');
 }
