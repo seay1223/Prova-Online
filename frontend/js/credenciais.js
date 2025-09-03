71e99ab44522585c5cfe328052b2684215588a57
Samuel <reis68535@gmail.com>
2025-09-03 12:17:08 -0300

    colocando credenciais reais

--- frontend/js/credenciais.js
+++ frontend/js/credenciais.js
@@ -1,6 +1,9 @@
 const CREDENCIAIS = {
-  alunos: ['mateus.rodrigues4@portalsesisp.org.br',
-    'samuel.reis@portalsesisp.org.br'],
+  alunos: {
+    turma: ["9B","9A","8B","8A","7A","7B","6A","6B"]
+
+  },
+
   professores: ['professor@escola.com'],
   senhaPadrao: 'Escolasesi123456'
 };
