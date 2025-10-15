bca808e009f8ea4c59e325997bfff7e38b5152f5
Samuel <reis68535@gmail.com>
2025-10-15 18:32:34 -0300

    erro de autentiaçao

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ app.use(
             secure: false,
             maxAge: 24 * 60 * 60 * 1000, // 24 horas
             httpOnly: true,
-            sameSite: "lax",
+            sameSite: "none",
             path: "/",
         },
         rolling: true,
